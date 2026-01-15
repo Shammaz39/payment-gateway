04ccc82292f178aa5452fe58a517e3fabad4021a
Ibrahim Shammaz <91185333+Shammaz39@users.noreply.github.com>
2026-01-15 14:55:23 +0530

    day 14.1 --> in local is done

--- notification-service/src/main/java/com/paymentgateway/notificationservice/client/MerchantClient.java
+++ notification-service/src/main/java/com/paymentgateway/notificationservice/client/MerchantClient.java
@@ -1,18 +1,25 @@
 package com.paymentgateway.notificationservice.client;
 
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
 @Service
 public class MerchantClient {
 
+    @Value("${merchant.service.url}")
+    private String merchantServiceUrl;
+
+
     private final RestTemplate restTemplate = new RestTemplate();
 
     public String fetchWebhookUrl(String email) {
 
+        String url = merchantServiceUrl + "/webhooks?email={email}";
+
         return restTemplate.getForObject(
-                "http://localhost:8081/api/v1/merchants/webhooks?email={email}",
+                url,
                 String.class,
                 email
         );
--- payment-processor-service/src/main/java/com/paymentgateway/paymentprocessor/service/BankClient.java
+++ payment-processor-service/src/main/java/com/paymentgateway/paymentprocessor/service/BankClient.java
@@ -3,24 +3,34 @@ package com.paymentgateway.paymentprocessor.service;
 import com.paymentgateway.mockbank.dto.BankPaymentRequest;
 import com.paymentgateway.mockbank.dto.BankPaymentResponse;
 import com.paymentgateway.paymentprocessor.entity.Transaction;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 @Service
 public class BankClient {
 
+    @Value("${processor.service.url}")
+    private String processorServiceUrl;
+
+    @Value("${mockbank.service.url}")
+    private String mockbankServiceUrl;
+
     private final RestTemplate restTemplate = new RestTemplate();
 
     public BankPaymentResponse initiatePayment(Transaction tx) {
+
+        String processCallBackUrl = processorServiceUrl+ "/callback";
+        String mockBankPayUrl = mockbankServiceUrl+ "/pay";
 
         BankPaymentRequest request = new BankPaymentRequest();
         request.setTransactionId(String.valueOf(tx.getId()));
         request.setAmount(tx.getAmount());
         request.setCallbackUrl(
-                "http://localhost:8083/processor/callback"
+                processCallBackUrl
         );
 
         return restTemplate.postForObject(
-                "http://localhost:8084/bank/pay",
+                mockBankPayUrl,
                 request,
                 BankPaymentResponse.class
         );
